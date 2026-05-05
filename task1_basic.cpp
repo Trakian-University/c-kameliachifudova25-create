9a2390d7ff1285b3d68f75bb5d0848da96ec2323
kameliachifudova25-create <kamelia.chifudova.25@trakia-uni.bg>
2026-05-05 09:38:08 +0300

    Update task1_basic.cpp

--- task1_basic.cpp
+++ task1_basic.cpp
@@ -6,11 +6,11 @@ int main() {
     cout << "Въведете число: ";
     cin >> n;
 
-    if (n % 2 = 0) {
+    if (n % 2 == 0) {
         cout << "Числото е четно" << endl;
     }
     else if (n % 2 != 0) {
-        cout << "Числото е нечетно" << endl
+        cout << "Числото е нечетно" << endl;
     }
 
     return 0;
